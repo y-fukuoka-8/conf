407068715b3cabc86019f753c9c17b794233992a
y-fukuoka <y-fukuoka@fujitsu.com>
2024-12-25 16:04:47 +0900

    first commit

---
+++ ex.py
@@ -0,0 +1,14 @@
+def ex_funcs(x, y):
+    return x + y
+
+
+def ex_funcs2(x, y):
+    return x - y
+
+
+def ex_funcs3(x, y):
+    return x * y
+
+
+def ex_funcs4(x, y):
+    return x / y
---
+++ test_ex.py
@@ -0,0 +1,23 @@
+import pytest
+
+from ex import ex_funcs
+
+# PDPM
+
+
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [
+        # この部分を変更
+        # PDPM
+        (3, 2, 1),
+        (10, 9, 8),
+        (11, 11, 11)
+    ],
+)
+def test_ex_funcs(x: int, y: int, expected: int) -> None:
+    assert ex_funcs(x, y) == expected
+    
+
+def test_ex_funcs(x: int, y: int, expected: int) -> None:
+
